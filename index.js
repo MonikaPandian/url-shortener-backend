9172479fd861925e3c546f0c5c7cf3a5d4c1fb9b
MonikaPandian <panmonikmm@gmail.com>
2022-11-08 20:09:58 +0530

    url-shortener

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ mongoose.connect(MONGO_URL,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> app.listen(PORT, ()=> console.log(`Server started. Listening at ${PORT}`)))
 .catch((error) => console.log(error));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "https://url-shortener-frontend-a1d3c9.netlify.app/");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
 app.use(express.json({
     extended: false
 }))
